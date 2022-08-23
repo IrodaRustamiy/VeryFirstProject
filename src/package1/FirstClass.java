58eb5308b0e3013d49ae482fa519a0cafe94f704
IrodaRustamiy <iroda.abdusalamova@gmail.com>
2022-08-23 11:10:31 -0500

    FirstClass.java updated

--- src/package1/FirstClass.java
+++ src/package1/FirstClass.java
@@ -3,5 +3,6 @@ package package1;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hi there");
+        System.out.println("How are you doing?");
     }
 }
