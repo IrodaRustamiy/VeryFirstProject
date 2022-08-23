0908211f9659fbd6852f0a8a8ac528212fdf5382
IrodaRustamiy <iroda.abdusalamova@gmail.com>
2022-08-23 11:10:52 -0500

    FirstClass.java completed

--- src/package1/FirstClass.java
+++ src/package1/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hi there");
         System.out.println("How are you doing?");
+        System.out.println("Bye then");
     }
 }
