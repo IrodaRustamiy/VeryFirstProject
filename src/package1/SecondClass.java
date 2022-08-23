6a9d14f949fba0f94d342e241c8a694f8151b65e
IrodaRustamiy <iroda.abdusalamova@gmail.com>
2022-08-23 11:12:01 -0500

    SecondClass.java created and updated

---
+++ src/package1/SecondClass.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Some text here");
+        System.out.println("Another message");
+    }
+}
